2c82f24b083e1c9262c6201343dbb1f02de914a0
Weslley Hendz <hendzweslley@gmail.com>
2019-12-14 11:34:34 -0300

    Meu primeiro projeto JS

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+var name=" Weslley Hendz";
+var idade= 29;
+var idade2= 10
+var frase = "Japão é o melhor time do mundo";
+
+// alert(name + " tem" +idade+" anos");
+
+console.log(name);
+console.log(frase.replace("Japão","Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
+
