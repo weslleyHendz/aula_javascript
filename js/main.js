d0d10ddf87dfb603a6c6211126053e47b4fcade0
Your name <hendzweslley@gmail.com>
2019-12-16 08:49:31 -0300

    segunda aula

--- js/main.js
+++ js/main.js
@@ -1,12 +1,50 @@
 
-var name=" Weslley Hendz";
-var idade= 29;
-var idade2= 10
-var frase = "Japão é o melhor time do mundo";
+var d = new Date();
+alert(d.getDay());
+
+/*
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+}
+*/
+/*
+var count = 0   ;
+while (count < 5){
+    console.log(count);
+    count = count + 1;
+}
+*/
+/*
+var idade = prompt("Qual sua idade")
+if(idade>=18){
+    alert("maior de idade");
+}
+else{
+    alert("menor de idade");
+}
+*/
+/*
+var fruta = {nome:"maça", cor: "vermelha"};
+console.log(fruta);
+alert(fruta.cor);
+*/
+
+//var lista = ["maçã", "pêra", "laranja"];
+//lista.push("uva")
+//lista.pop();
+
+//console.group(lista.);
+
+
+//var name=" Weslley Hendz";
+//var idade= 29;
+//var idade2= 10
+//var frase = "Japão é o melhor time do mundo";
 
 // alert(name + " tem" +idade+" anos");
 
-console.log(name);
-console.log(frase.replace("Japão","Brasil"));
+//console.log(name);
+//console.log(frase.replace("Japão","Brasil"));
 //alert(frase.replace("Japão", "Brasil"));
 
