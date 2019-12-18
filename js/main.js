8da3a15bc3e2e5ff487cd9dfc6cf8d3161efa6fc
Weslley Hendz <hendzweslley@gmail.com>
2019-12-18 20:31:11 -0300

    Final

--- js/main.js
+++ js/main.js
@@ -1,7 +1,57 @@
 
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar<b>";
+        //console.log (document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar (){
+    window.open("https://globallabs.academy/");
+    //window.location.href = "https://globallabs.academy/"
+
+}
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    elemento.innerHTML = "Obrigado por passar o mouse"
+    //alert("trocar texto")
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    
+    elemento.innerHTML = "Passe o mouse aqui";
+} 
+function load(){
+    alert("página carregada");
+}
+function funcaoChange(elemento){
+    console.log(elemento.value);
+
+}
+
+/*function soma(n1, n2){
+    return n1+n2;
+}
+*/
+/*
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true; 
+    } else{
+        validar = false;
+    }
+    return validar;
+}
+var idade = prompt ("Qual sua idade")
+console.log(validaIdade(idade));
+*/
+//alert(soma(5, 10));
+
+    /*
 var d = new Date();
 alert(d.getDay());
-
+*/
 /*
 var count;
 for(count=0; count <= 5; count++){
